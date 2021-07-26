79507b9abbf76af8ce871e3763e612537b6b84ad
HiChen85 <hccc_zhang@outlook.com>
2021-07-26 15:56:05 +0100

    finish Chapter5 exercise

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Category, Page
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'views', 'likes']
+    list_display_links = ['name',]
+    list_editable = ['views', 'likes',]
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'url']#, 'views']
+    list_display_links = ['title',]
+    # list_editable = ['category', 'url']
+
+    list_filter = ['category',]
+    # search_fields = ['category__name', 'title']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(verbose_name="category", max_length=128, unique=True)
+    views = models.IntegerField(verbose_name='views', default=0)
+    likes = models.IntegerField(verbose_name='likes', default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        verbose_name_plural = 'Categories'
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="title", max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(verbose_name="views", default=0)
+
+    def __str__(self):
+        return self.title
